8189b4dccc7b49ba6cdee471cf77269b5f8b83e1
Soundarya <soundaryakishore05@gmail.com>
2026-06-24 15:00:53 -0700

    Add calculator and search tools with error handling

--- tools.py
+++ tools.py
@@ -1,4 +1,8 @@
 from simpleeval import simple_eval
+from dotenv import load_dotenv
+from tavily import TavilyClient
+import os
+load_dotenv()
 def calculator(expression):
     try:
         
@@ -10,6 +14,17 @@ def calculator(expression):
 
 print(calculator("18 * 4500"))
 print(calculator("18 % of 4500"))
+
+def search_engine(question):
+    try:
+        tavily_client = TavilyClient(api_key=os.environ.get("TAVILY_API_KEY"))
+        response = tavily_client.search(question)
+        return response["results"][0]["content"]
+    
+    except Exception as e:
+        return f"An unexpected error occured: {e}"
+
+print(search_engine("Who is Leo Messi?"))
 
 
     
